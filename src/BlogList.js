06bb7a40d86c794a15c95bd88f615353b5a82d1b
ThanhNghi <nghilt19411@gmail.com>
2022-01-16 20:49:15 +0700

    add json server

---
+++ src/BlogList.js
@@ -0,0 +1,24 @@
+
+
+const BlogList = ( {blogs , title, handleDelete }) => {
+ 
+
+    return ( 
+        <div className="blog-list">
+        <h2>{title}</h2>
+              <div className="home">
+            {blogs.map( (blog)=>(
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                    <button onClick={ () => handleDelete(blog.id) } > Delete blog</button>
+                </div>
+            )) }
+           
+        </div>
+        </div>
+
+     );
+}
+ 
+export default BlogList ;
--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,41 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import BlogList from './BlogList';
 const Home = () => {
    // let name = 'mario';
-    const [blogs , setBlogs ] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ])
-   
-
-
+    const [blogs , setBlogs ] = useState(null);
+    const [isPending , setIsPending] = useState(true);
+    const [error , setError] = useState(true)
+    useEffect( () => {
+       setTimeout(()=>{
+        fetch('http://localhost:8000/blogs')
+            .then( res =>{
+                if(!res.ok){
+                    throw Error('Could not fetch data');
+                }
+                return res.json();
+            })
+            .then(data =>{
+                setBlogs(data)
+                setIsPending(false)
+                setError(null);
+            })
+            .catch(err=>{
+                setError(err.message);
+                setIsPending(false)
+            })
+       },1000)
+    },[]);
     return (  
         <div className="home">
-            {blogs.map( (blog)=>(
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>Written by {blog.author}</p>
-                </div>
-            )) }
-           
+           {error && <div>{error}</div>  }
+            {isPending && <div>Loading...</div>  }
+             {blogs && <BlogList blogs = {blogs} 
+                        title="All Blogs!" 
+                        
+            />}
+     
         </div>
+    
     );
 }
  
